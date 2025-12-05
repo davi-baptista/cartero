31bff3449c5aae71f97811594bf1bab0f0bb1f2e
Davi <inovacao@controller-rnc.com.br>
2025-12-05 17:43:21 -0300

    chore(config): fix Nest entryfile and vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
   test: {
     globals: true,
+    root: './',
   },
+  plugins: [
+    tsConfigPaths(),
+  ],
 })
